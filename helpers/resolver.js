3fa4ac3efee5786c06ffef135e51492218e8e272
ncaringi <caringi.noe@gmail.com>
2024-02-19 10:24:30 +0100

    test résolution maze 1

--- helpers/resolver.js
+++ helpers/resolver.js
@@ -1,4 +1,5 @@
 exports.resolver = function (maze, start, end) {
     console.log('Resolve the path of the maze here');
+    console.log("La Case de départ du maze est : "+maze[start].x+";"+maze[start].y);
 
 }
